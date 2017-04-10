f4ce5b3fdfa6716533f948fee31693617915810f
Edvin Dizdarevic <dizdarevic1994@gmail.com>
2017-04-10 19:06:48 -0400

    MySQL queries done and explain plans for 4-6

--- sqlFiles/BitsPlease_AftTuning_MySQL.sql
+++ sqlFiles/BitsPlease_AftTuning_MySQL.sql
@@ -1,5 +1,26 @@
 USE proj4_keys; #database with keys/indexes 
 
+#Author of queries 1 - 3: Edvin Dizdarevic
+
+#Query1: List the name of the student with id equal to v1 (id).
+SELECT s.sname 
+FROM STUDENT AS s 
+WHERE s.id = '545899';
+
+#Query2: List the names of students with id in the range of v2 (id) to v3 (inclusive).
+SELECT s.sname
+FROM STUDENT AS s
+WHERE s.id BETWEEN '15192' AND '17138';
+
+#Query3: List the names of students who have taken course v4 (crsCode).
+SELECT s.sname
+FROM STUDENT AS s
+WHERE s.id = ANY (SELECT sid.studId
+            FROM TRANSCRIPT AS sid
+            WHERE sid.crsCode = 'crsCode902901');
+
+#Author of queries 4 - 6: Jonathan Waring
+
 #Query 4: List the names of students who have taken a course taught by professor v5 (name)
 SELECT s.sname
 FROM STUDENT AS s INNER JOIN TRANSCRIPT AS r ON r.studId=s.id INNER JOIN TEACHING AS t ON r.crsCode=t.crsCode
@@ -18,6 +39,6 @@ FROM STUDENT AS s
 WHERE(SELECT COUNT(c.crsCode) AS cnt
 	  FROM STUDENT AS s INNER JOIN TRANSCRIPT AS t ON t.studId=s.id INNER JOIN COURSE AS c ON t.crsCode=c.crsCode
 	  WHERE c.deptId = 'deptId424969'
-	  GROUP BY t.studId
+	  GROUP BY s.sname
       HAVING cnt = (SELECT COUNT(crsCode) FROM COURSE WHERE deptId = 'deptId424969')
 	)
--- sqlFiles/BitsPlease_BefTuning_MySQL.sql
+++ sqlFiles/BitsPlease_BefTuning_MySQL.sql
@@ -1,5 +1,26 @@
 USE proj4; #database without keys/indexes 
 
+#Author of queries 1 - 3: Edvin Dizdarevic
+
+#Query1: List the name of the student with id equal to v1 (id).
+SELECT sname 
+FROM STUDENT
+WHERE id = '545899';
+
+#Query2: List the names of students with id in the range of v2 (id) to v3 (inclusive).
+SELECT sname 
+FROM STUDENT
+WHERE id BETWEEN '15192' AND '17138';
+
+#Query3: List the names of students who have taken course v4 (crsCode).
+SELECT sname
+FROM STUDENT 
+WHERE id = ANY (SELECT studId 
+            FROM TRANSCRIPT 
+            WHERE crsCode = 'crsCode902901');
+
+#Author of queries 4 - 6: Jonathan Waring
+
 #Query 4: List the names of students who have taken a course taught by professor v5 (491584)
 SELECT s.sname
 FROM STUDENT AS s, TRANSCRIPT AS r, TEACHING AS t
@@ -16,5 +37,5 @@ WHERE t.crsCode=c.crsCode AND c.deptId='deptId664077' AND t.studId=s.id AND c.de
 SELECT s.sname 
 FROM STUDENT AS s, COURSE AS c, TRANSCRIPT AS t
 WHERE t.crsCode=c.crsCode AND t.studId=s.id AND c.deptId='deptId424969'
-GROUP BY s.id
+GROUP BY s.sname
 HAVING COUNT(*) = (SELECT COUNT(*) FROM COURSE WHERE COURSE.deptId='deptId424969');
